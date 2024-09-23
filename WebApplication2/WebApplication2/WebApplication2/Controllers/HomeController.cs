1d9af0ff17cdabc24babeb546c2c10b98ea6568b
JCB <39355117+jcbthegreat@users.noreply.github.com>
2024-09-23 11:46:57 +0800

    Add files via upload

---
+++ WebApplication2/WebApplication2/WebApplication2/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication2.Controllers
+{
+    [ApiController]
+    [Route("{*catchall}")]
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        [HttpPost]
+        public async Task<IActionResult> HandleRequest(string catchall)
+        {
+            if (HttpContext.Request.Method == HttpMethods.Get)
+            {
+                var queryParams = HttpContext.Request.Query;
+                                
+
+                Console.WriteLine("Method: GET");
+                Console.WriteLine($"Caught Route: {catchall}");
+
+                foreach (var item in queryParams)
+                {
+                    Console.WriteLine($"Query Param: {item.Key} = {item.Value}");
+                }
+
+                return Ok(new
+                {
+                    Method = "GET",
+                    CaughtRoute = catchall,
+                    QueryStringData = queryParams
+                });
+            }
+            else
+            {
+                return BadRequest("Unsupported Method");
+            }
+        }
+    }
+
+}
